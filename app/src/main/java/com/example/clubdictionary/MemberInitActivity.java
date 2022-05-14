ced0e7c7b4976e5de57e6e7a229c77bcea6d95be
jyj10 <jyj10201000@gmail.com>
2022-05-14 15:37:44 +0900

    user db연결 제외  완료 - 윤진 5/14

--- app/src/main/java/com/example/clubdictionary/MemberInitActivity.java
+++ app/src/main/java/com/example/clubdictionary/MemberInitActivity.java
@@ -2,7 +2,11 @@ package com.example.clubdictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MemberInitActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MemberInitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_init);
+        Button startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MemberInitActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                toast("신비한 동아리 사전에 오신 것을 환영합니다!!");
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void toast(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
