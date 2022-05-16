44a2ae19a9e6069fca25a9e0d90e5d541d36ac2c
jyj10 <jyj10201000@gmail.com>
2022-05-16 22:42:57 +0900

    upgrade android studio gradle maybe changed -윤진 5/16

--- app/src/main/java/com/example/clubdictionary/MyPageFragment.java
+++ app/src/main/java/com/example/clubdictionary/MyPageFragment.java
@@ -65,7 +65,7 @@ public class MyPageFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
-                getActivity().finish();
+                Objects.requireNonNull(getActivity()).finish();
             }
         });
 
@@ -76,7 +76,7 @@ public class MyPageFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
-                getActivity().finish();
+                Objects.requireNonNull(getActivity()).finish();
             }
         });
 
