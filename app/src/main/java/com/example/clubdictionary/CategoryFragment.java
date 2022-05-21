794829bbb20cf0ca45419bb79412813b120ac274
jyj10 <jyj10201000@gmail.com>
2022-05-21 22:43:40 +0900

    commit 5/21 윤진

--- app/src/main/java/com/example/clubdictionary/CategoryFragment.java
+++ app/src/main/java/com/example/clubdictionary/CategoryFragment.java
@@ -1,9 +1,11 @@
 package com.example.clubdictionary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +15,25 @@ public class CategoryFragment extends Fragment {
 
     public CategoryFragment() {
     }
+//
+TextView society, study, sports, culture, religion;;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        society = view.findViewById(R.id.society);
+        study = view.findViewById(R.id.study);
+        sports = view.findViewById(R.id.sports);
+        culture = view.findViewById(R.id.culture);
+        religion = view.findViewById(R.id.religion);
+        society.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SocietyFragment.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/clubdictionary/SocietyFragment.java
@@ -0,0 +1,30 @@
+package com.example.clubdictionary;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SocietyFragment extends Fragment {
+
+
+    public SocietyFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_society, container, false);
+
+
+
+        return view;
+    }
+}
