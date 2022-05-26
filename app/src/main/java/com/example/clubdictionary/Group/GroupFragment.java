f99b65cd3e5b04573f709f39700e67d5ac7bca0a
baha1909 <baha1909@naver.com>
2022-05-26 17:15:57 +0900

    네비게이션 메뉴 추가

---
+++ app/src/main/java/com/example/clubdictionary/Group/GroupFragment.java
@@ -0,0 +1,30 @@
+package com.example.clubdictionary.Group;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clubdictionary.Home.HomeRecyclerViewAdapter;
+import com.example.clubdictionary.R;
+
+import java.util.ArrayList;
+
+public class GroupFragment extends Fragment {
+
+    public GroupFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_group, container, false);
+        return view;
+    }
+}
